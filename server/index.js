59bfe8dc1fcefccaa2e85f449eb30af68dcacbd6
Ilhyo Sung <sih8189@gmail.com>
2020-06-15 02:10:33 -0400

    added production setting and favicon

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -8,15 +8,16 @@ const config = require('./config/key');
 const { User } = require('./models/User');
 const { auth } = require('./middleware/auth');
 
-// application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
+const port = process.env.PORT || 5000;
 
-// application/json
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 const mongoose = require('mongoose');
+
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -30,6 +31,20 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
 
 app.get('/', (req, res) => res.send('Hello World! Nice to meet you'));
 
@@ -93,9 +108,3 @@ app.get('/api/users/logout', auth, (req, res) => {
     return res.status(200).send({ logoutSuccess: true });
   });
 });
-
-const port = 5000;
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
