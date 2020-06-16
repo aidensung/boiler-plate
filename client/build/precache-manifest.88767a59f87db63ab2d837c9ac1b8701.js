e520b878bcf5247c08fad0d6a8bfe9dc1ba1cd7b
Ilhyo Sung <sih8189@gmail.com>
2020-06-16 01:16:19 -0400

    fixed

---
+++ client/build/precache-manifest.88767a59f87db63ab2d837c9ac1b8701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70fabc367994b31cb2c5bee588e3bfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10e4960397bb7fc8e6a",
+    "url": "/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "8618db15a98f4e8a0dc2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e10e4960397bb7fc8e6a",
+    "url": "/static/js/2.19e8d808.chunk.js"
+  },
+  {
+    "revision": "a680856a4903381d69a83276068aa227",
+    "url": "/static/js/2.19e8d808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8618db15a98f4e8a0dc2",
+    "url": "/static/js/main.d0b76b22.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
