4ad8d1ba3a5a1961a476bec62209f36bb694f582
Ilhyo Sung <sih8189@gmail.com>
2020-06-20 16:06:21 -0400

    initialized heroku deployment

--- server/index.js
+++ server/index.js
@@ -32,13 +32,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', function (req, res) {
+//     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//   });
+// }
 
 app.listen(port, (error) => {
   if (error) throw error;
